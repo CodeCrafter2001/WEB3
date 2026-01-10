a6b8ff6d8bf845771aac54390fa2fb14b419bb2b
CodeCrafter2001 <anmolbansiwal1@gmail.com>
2026-01-10 09:58:35 +0530

    installed libraries

---
+++ web3-token-launchpad/1-token-launchpad-starter/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import { TokenLaunchpad } from './components/TokenLaunchpad'
+
+function App() {
+  return (
+    <TokenLaunchpad></TokenLaunchpad>
+  )
+}
+
+export default App
+ 
