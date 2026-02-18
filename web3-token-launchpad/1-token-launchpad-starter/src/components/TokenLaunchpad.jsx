2e69dde604792e1456ed613602c11936cfd855a1
CodeCrafter2001 <anmolbansiwal1@gmail.com>
2026-02-18 23:47:03 +0530

    created mint function

--- web3-token-launchpad/1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
+++ web3-token-launchpad/1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
@@ -1,13 +1,27 @@
 import { Keypair, SystemProgram, Transaction } from "@solana/web3.js";
 import { useConnection, useWallet } from '@solana/wallet-adapter-react';
-import { MINT_SIZE, TOKEN_2022_PROGRAM_ID, createInitializeMint2Instruction, createMint, getMinimumBalanceForRentExemptMint } from "@solana/spl-token"
+import { MINT_SIZE, TOKEN_2022_PROGRAM_ID, TOKEN_PROGRAM_ID, createInitializeMint2Instruction, createMint, getMinimumBalanceForRentExemptMint } from "@solana/spl-token"
 export function TokenLaunchpad() {
     const { connection } = useConnection();
     const wallet = useWallet();
     async function createToken() {
         const mintKeyPair = Keypair.generate();
         const lamports = await getMinimumBalanceForRentExemptMint(connection);
-        
+
+        const transaction = new Transaction().add(SystemProgram.createAccount({
+            fromPubkey : wallet.publicKey,
+            newAccountPubkey: mintKeyPair.publicKey,
+            space: MINT_SIZE,
+            lamports,
+            programId : TOKEN_PROGRAM_ID,
+        }),
+        createInitializeMint2Instruction(mintKeyPair.publicKey,9, wallet.publicKey, wallet.publicKey, TOKEN_PROGRAM_ID) 
+    );
+    transaction.feePayer= wallet.publicKey;
+    transaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+    transaction.partialSign(mintKeyPair);
+    await wallet.sendTransaction(transaction, connection);
+    console.log(`token mint created at ${mintKeyPair.publicKey.toBase58()}`);
 
     }
     return <div style={{
